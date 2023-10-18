35dc85c42554508ee581dfdf548231206d42ec72
PSzew <119743585+PSzew@users.noreply.github.com>
2023-10-18 14:44:00 +0200

    Update ArduinoCode.ino

--- ArduinoCode.ino
+++ ArduinoCode.ino
@@ -1,12 +1,60 @@
 #include <Servo.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <Buzzer.h>
+
 Servo myservo;
 int message = 0;
 int pos = 0; 
+LiquidCrystal_I2C lcd(0x27,20,4);
+String pin = "";
+Buzzer buzzer(10, 13);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   myservo.attach(9);
+  myservo.write(0);
+  lcd.init();
+  lcd.backlight();
+  lcdTemplate();
+  pinMode(10,OUTPUT);
+}
+
+void lcdTemplate()
+{
+  pin = "";
+  lcd.clear(); 
+  lcd.setCursor(3,0);
+  lcd.print("Twoj Pin:");
+  lcd.setCursor(2,1);
+  lcd.print(pin);
+}
+
+void lcdTemplate(char nextChar)
+{
+  lcd.clear();
+  pin = pin + " " + nextChar; 
+  lcd.setCursor(3,0);
+  lcd.print("Twoj Pin:");
+  lcd.setCursor(2,1);
+  lcd.print(pin);
+}
+
+void buzzerMelody()
+{
+  buzzer.begin(100);
+  buzzer.sound(NOTE_E7, 80);
+  buzzer.sound(NOTE_E7, 80);
+  buzzer.sound(0, 80);
+  buzzer.sound(NOTE_E7, 80);
+  buzzer.sound(0, 80);
+  buzzer.sound(NOTE_C7, 80);
+  buzzer.sound(NOTE_E7, 80);
+  buzzer.sound(0, 80);
+  buzzer.sound(NOTE_G7, 80);
+  buzzer.sound(0, 240);
+  buzzer.sound(NOTE_G6, 80);
 }
 
 void loop() {
@@ -14,24 +62,68 @@ void loop() {
   if (Serial.available() > 0)
   {
     message = Serial.read();
-    
     Serial.print("Recieved: ");
     Serial.println((char)message);
-    if((char)message == 'a')
+    if((char)message == 'b')
     {
-      for (pos = 0; pos <= 180; pos += 1) 
+      for (pos = 0; pos <= 130; pos += 1) 
       { 
         myservo.write(pos);              
-        delay(15);                       
+        delay(10);                       
       }
     }
-    else if((char)message == 'b')
+    else if((char)message == 'a')
     {
-      for (pos = 180; pos >= 0; pos -= 1) 
+      buzzerMelody();
+      lcd.clear();
+      lcd.setCursor(5,0);
+      lcd.print("Sukces");
+      lcd.setCursor(3,1);
+      lcd.print("Otwieranie");
+      
+      for (pos = 130; pos >= 0; pos -= 1) 
+      {
+        myservo.write(pos);              
+        delay(10);                      
+      }  
+    }
+    else if((char)message == 'e')
+    {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Nie poprawny pin");
+      digitalWrite(10,HIGH);
+      delay(900);
+      digitalWrite(10,LOW);
+
+      //otwieranie
+      for (pos = 130; pos >= 55; pos -= 1) 
       { 
           myservo.write(pos);              
-          delay(15);                       
-        }  
+          delay(10);                       
+      }
+
+      delay(650);
+
+      //zamykanie
+      for (pos = 55; pos <= 130; pos += 1) 
+      { 
+        myservo.write(pos);              
+        delay(10);                       
+      }
+      lcdTemplate();
+
+    }
+    else if((char)message == 'c')
+    {
+      lcdTemplate();
+    }
+    else if (message !=10)
+    {
+      lcdTemplate((char)message);
+      digitalWrite(10,HIGH);
+      delay(100);
+      digitalWrite(10,LOW);
     }
   }
 }
